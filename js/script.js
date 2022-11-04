83b4beb8cb7af8a8dcc6625053635476f6f0cd2b
ashley-monaghan <ashley-monaghan@users.noreply.github.com>
2022-11-04 13:42:38 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ function calculate() {
   const fahrenheit = parseInt(document.getElementById("fahrenheit").value)
 
   // process
-  const math = (fahrenheit - 32) * 5/9
+  const math = ((fahrenheit - 32) * 5) / 9
   // output
   document.getElementById("math").innerHTML = "Celsius: " + math + " C°"
 }
